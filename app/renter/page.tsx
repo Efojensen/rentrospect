54830fd32a83e3c938fe62e6898508c3205c7ae4
EfoJensen <Jensena231@gmail.com>
2026-05-07 15:36:09 +0000

    added arrows but yet to make them functional

--- app/renter/page.tsx
+++ app/renter/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { assets } from "@/constants/assets";
 import AssetTile from "@/components/AssetTile";
 import HeroPicture from "@/components/HeroPicture";
@@ -8,8 +9,8 @@ import { categories } from "@/constants/categories";
 
 export default function Home() {
     return (
-        <main className='flex flex-col'>
-            <div className='flex gap-3.25 md:gap-5.25 overflow-x-auto whitespace-nowrap mx-auto mb-4 md:mb-6 scrollbar-hide w-full md:justify-center'>
+        <main className='flex flex-col gap-4 md:gap-6'>
+            <div className='flex gap-3.25 md:gap-5.25 overflow-x-auto whitespace-nowrap mx-auto scrollbar-hide w-full md:justify-center'>
                 {categories.map((category, index) => (
                     <div key={index} className='shrink-0'>
                         <CategoryTile
@@ -20,10 +21,32 @@ export default function Home() {
                 ))}
             </div>
             <HeroPicture />
+
+            {/* For rendering the title and arrows for moving the scrollbar */}
             <div className='flex justify-between items-center'>
                 <h2 className='dmSans-font text-[1.25rem] font-medium capitalize text-sectionTitle p-2.5'>Assets Nearby</h2>
+                <div className='flex gap-5'>
+                    <div className='flex size-10 bg-arrowBackground rounded-xl items-center justify-center'>
+                        <Image
+                            width={16}
+                            height={16}
+                            alt='left-arrow'
+                            src='/svgs/chevron_left.svg'
+                            className='size-3'
+                            />
+                    </div>
+                    <div className='flex size-10 bg-arrowBackground rounded-xl items-center justify-center'>
+                        <Image
+                            width={16}
+                            height={16}
+                            alt='left-arrow'
+                            className='size-3'
+                            src='/svgs/chevron_right.svg'
+                        />
+                    </div>
+                </div>
             </div>
-            <div className='flex mt-7.5 gap-5 overflow-x-auto scrollbar-hide whitespace-nowrap'>
+            <div className='flex gap-5 overflow-x-auto scrollbar-hide whitespace-nowrap'>
                 {assets.map((asset, index) => (
                     <div key={index} className='shrink-0'>
                         <AssetTile
@@ -40,7 +63,32 @@ export default function Home() {
                     </div>
                 ))}
             </div>
-            <div className='flex mt-7.5 gap-5 overflow-x-auto whitespace-nowrap'>
+
+            {/* For rendering the title and arrows for moving the scrollbar */}
+            <div className='flex justify-between items-center'>
+                <h2 className='dmSans-font text-[1.25rem] font-medium capitalize text-sectionTitle p-2.5'>new arrivals</h2>
+                <div className='flex gap-5'>
+                    <div className='flex size-10 bg-arrowBackground rounded-xl items-center justify-center'>
+                        <Image
+                            width={16}
+                            height={16}
+                            alt='left-arrow'
+                            src='/svgs/chevron_left.svg'
+                            className='size-3'
+                            />
+                    </div>
+                    <div className='flex size-10 bg-arrowBackground rounded-xl items-center justify-center'>
+                        <Image
+                            width={16}
+                            height={16}
+                            alt='left-arrow'
+                            className='size-3'
+                            src='/svgs/chevron_right.svg'
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className='flex gap-5 overflow-x-auto whitespace-nowrap'>
                 {arrivals.map((arrival, index) => (
                     <div key={index} className='shrink-0'>
                         <ArrivalTile
@@ -52,6 +100,30 @@ export default function Home() {
                         />
                     </div>
                 ))}
+            </div>
+            {/* For rendering the title and arrows for moving the scrollbar */}
+            <div className='flex justify-between items-center'>
+                <h2 className='dmSans-font text-[1.25rem] font-medium capitalize text-sectionTitle p-2.5'>top selling categories</h2>
+                <div className='flex gap-5'>
+                    <div className='flex size-10 bg-arrowBackground rounded-xl items-center justify-center'>
+                        <Image
+                            width={16}
+                            height={16}
+                            alt='left-arrow'
+                            src='/svgs/chevron_left.svg'
+                            className='size-3'
+                            />
+                    </div>
+                    <div className='flex size-10 bg-arrowBackground rounded-xl items-center justify-center'>
+                        <Image
+                            width={16}
+                            height={16}
+                            alt='left-arrow'
+                            className='size-3'
+                            src='/svgs/chevron_right.svg'
+                        />
+                    </div>
+                </div>
             </div>
         </main>
     );
